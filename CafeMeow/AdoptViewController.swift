435eebdb384644962ead547cad819be424f911da
Anna Coulter <arcoulter2@mail.bradley.edu>
2019-04-19 21:30:53 -0500

    catching up sorry i suck

---
+++ CafeMeow/AdoptViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AdoptViewController.swift
+//  CafeMeow
+//
+//  Created by Anna Coulter on 4/19/19.
+//  Copyright © 2019 Anna Coulter. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class AdoptViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func didPressGoBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
